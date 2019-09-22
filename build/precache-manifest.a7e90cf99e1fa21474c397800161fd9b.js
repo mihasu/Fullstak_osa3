dabe3484d48ce80ceda47e0955b0d3cf64148d9f
Mikael Hasu <mikael.hasu@helsinki.fi>
2019-09-22 16:36:34 +0300

    build

---
+++ build/precache-manifest.a7e90cf99e1fa21474c397800161fd9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7932e1e82bbb4c1786d0d9228b6b42fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55fe0564f4d58d3288f0",
+    "url": "/static/css/main.53724732.chunk.css"
+  },
+  {
+    "revision": "a0210ab1b5b72d3108e6",
+    "url": "/static/js/2.09ccb4a8.chunk.js"
+  },
+  {
+    "revision": "55fe0564f4d58d3288f0",
+    "url": "/static/js/main.062c54dc.chunk.js"
+  },
+  {
+    "revision": "bd15de2c81a3438b90f1",
+    "url": "/static/js/runtime~main.11bda596.js"
+  }
+]);
